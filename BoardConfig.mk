1d29fd319823045027af95a5d6393bda5e6031e4
leskal <alexakis1997@gmail.com>
2018-01-17 18:43:28 +0200

    oxygen: New tree format pt.1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,26 +16,10 @@
 
 LOCAL_PATH := device/xiaomi/oxygen
 
+# Device board elements
+include $(LOCAL_PATH)/PlatformConfig.mk
+
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
-
-# Architecture
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53
-
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
-TARGET_BOARD_PLATFORM := msm8953
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
-
-TARGET_BOARD_SUFFIX := _64
-TARGET_USES_64_BIT_BINDER := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
